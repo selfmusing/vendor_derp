b287a0a8b4669a0091d4c9d9ba68ca98c341ff43
nift4 <nift4@protonmail.com>
2023-04-14 00:37:39 +0200

    config: Remove useless addon.d file
    * Avoid detection as custom ROM
    
    Change-Id: I1fbd63cc116d53c7c366662b632163f6bc079bbf

--- config/common.mk
+++ config/common.mk
@@ -29,10 +29,6 @@ ifneq ($(TARGET_EXCLUDE_BACKUPTOOL),true)
 PRODUCT_COPY_FILES += \
     vendor/derp/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/derp/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/derp/prebuilt/common/bin/50-derp.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-derp.sh
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/addon.d/50-derp.sh
 
 ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
 PRODUCT_COPY_FILES += \
