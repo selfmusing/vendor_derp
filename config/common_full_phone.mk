bef42c81d43cf5f4c81070e105f439a045cbdb9e
selfmusing <mirandamehek@gmail.com>
2024-05-29 03:27:15 +0530

    config: Nuke UDFPS Animation Support

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,10 +1,6 @@
 # Inherit mobile full common DerpFest stuff
 $(call inherit-product, vendor/derp/config/common_mobile_full.mk)
 
-# UDFPS Animation effects
-PRODUCT_PACKAGES += \
-    UdfpsAnimations
-
 # Enable support of one-handed mode
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.support_one_handed_mode?=true
